e754f222df8769a7738784924b2da570438da905
JeffreyDettmann <jeffrey.dettmann@gmail.com>
2023-07-10 14:46:13 -0400

    REG-21 improve appearance on mobile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def divided_content(content)
+    content&.gsub("\n", '<br/>')
+  end
+  
   def class_for_tournament(tournament)
-    "aoe#{tournament.game[15..].downcase}" if tournament.game
+    if tournament.game == 'Age of Mythology'
+      'aom'
+    elsif tournament.game
+      "aoe#{tournament.game[15..].downcase}"
+    end
   end
 end
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+Date::DATE_FORMATS[:day_month] = '%-d %B'
