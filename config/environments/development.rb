4755ec1500ffe653cca1e52c52837ed1276c2702
JeffreyDettmann <jeffrey.dettmann@gmail.com>
2023-08-01 11:45:34 -0400

    REG-27 add circleci config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,7 +73,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  # Show everything in the web console
-  config.web_console.permissions = '192.168.0.0/16'
 end
---
+++ lib/tasks/rubocop.rake
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+if Gem::Specification.find_all_by_name('rubocop').present?
+  require 'rubocop/rake_task'
+  RuboCop::RakeTask.new
+end
